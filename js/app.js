44252a4f1105fa4a97d36db128084b544e5b37e8
Prince Paraste <princeparaste78@gmail.com>
2020-02-18 16:14:51 +0530

    18-02-2020

---
+++ js/app.js
@@ -0,0 +1,56 @@
+// $(function () {
+//     $(window).on('scroll', function () {
+//         if ( $(window).scrollTop() > 10 ) {
+//             $('.navbar').addClass('active');
+//         } else {
+//             $('.navbar').removeClass('active');
+//         }
+//     });
+// });
+
+
+
+/* Smooth Scroll */
+
+// $('navbar a').on('click', function(e){
+// 	console.log("Fsd");
+// 	console.log(this.hash);
+// 	if(this.hash !== ''){
+// 		e.preventDefault();
+
+// 		const hash = this.hash;
+
+// 		$('html,body').animate(
+// 		{
+// 			scrollTop: $(hash).offset().top
+// 		},
+// 		800
+// 		);
+// 	}
+// });
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 400, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
